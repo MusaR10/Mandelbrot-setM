1d26425b6b496f35c4889519a0b4ed1825d7fd35
Karnpreet2 <166642153+Karnpreet2@users.noreply.github.com>
2024-04-22 20:40:47 -0700

    Update ComplexPlane.cpp

--- ComplexPlane.cpp
+++ ComplexPlane.cpp
@@ -1,4 +1,5 @@
 #include "ComplexPlane.h"
+#include <complex>
 
 ComplexPlane::ComplexPlane(int pixelWidth, int pixelHeight){
     m_pixel_size = {pixelWidth, pixelHeight};
@@ -7,9 +8,8 @@ ComplexPlane::ComplexPlane(int pixelWidth, int pixelHeight){
     m_plane_size = {BASE_WIDTH, BASE_HEIGHT * m_aspectRatio};
     m_ZoomCount = 0;
     m_state = State::CALCULATING;
-    m_vArray.setPrimitiveType(Points);
-    m_vArray.resize(pixelWidth * pixelHeight);
-    m_vArray.create(); 
+    m_vArray = VertexArray(Points, pixelWidth*pixelHeight);
+    court<<m_plane_size.x << " " << m_plane_size.y << endl;
 }
 
 void ComplexPlane::draw(RenderTarget& target, RenderStates states) const{
@@ -20,8 +20,8 @@ void ComplexPlane::draw(RenderTarget& target, RenderStates states) const{
 
 void ComplexPlane::zoomIn(){
     m_ZoomCount++;
-    float x_size = BASE_WIDTH * pow(BASE_ZOOM, m_ZoomCount);
-    float y_size = BASE_HEIGHT * m_aspectRatio * pow(BASE_ZOOM, m_ZoomCount);
+    float x_size = BASE_WIDTH * (pow(BASE_ZOOM, m_ZoomCount));
+    float y_size = BASE_HEIGHT * m_aspectRatio * (pow(BASE_ZOOM, m_ZoomCount));
     m_plane_size = {x_size, y_size};
     m_state = State::CALCULATING;
 }
@@ -32,5 +32,18 @@ void ComplexPlane::zoomOut(){
   float y_size = BASE_HEIGHT * m_aspectRatio * pow(BASE_ZOOM, m_ZoomCount);
   m_plane_size = {x_size, y_size};
   m_state = State::CALCULATING;
+}
+
+void ComplexPlane::loadText(Text& text){
+    stringstream ss;
+    ss<< "Mandelbrot set\n";
+    ss<< "Center: " << m_plane_center.x << ", " << m_plane_center.y << "\n";
+    ss<< "Size: " << m_plane_size.x << ", " << m_plane_size.y<< "\n";
+    ss<< "Left click to zoom in\n";
+    ss<< "Right click to zoom out\n";
+    text.setString(ss.str());
+    text.setCharacterSize(20);
+    text.setFillColor(Color::White);
+    text.setPosition(10,10);
 }
 
