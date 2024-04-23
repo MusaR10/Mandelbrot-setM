859b3f181041886123b66b42e55972100c793e70
Karnpreet2 <166642153+Karnpreet2@users.noreply.github.com>
2024-04-22 18:33:13 -0700

    Update ComplexPlane.cpp

--- ComplexPlane.cpp
+++ ComplexPlane.cpp
@@ -5,11 +5,11 @@ ComplexPlane::ComplexPlane(int pixelWidth, int pixelHeight){
     m_aspectRatio = (float)pixelHeight / (float)pixelWidth;
     m_plane_center = {0, 0};
     m_plane_size = {BASE_WIDTH, BASE_HEIGHT * m_aspectRatio};
-    m_zoomCount = 0;
-    m_State = State::CALCULATING;
-    m_vertexArray.setPrimitiveType(Points);
-    m_vertexArray.resize(pixelWidth * pixelHeight);
-    m_vertexArray.create(); 
+    m_ZoomCount = 0;
+    m_state = State::CALCULATING;
+    m_vArray.setPrimitiveType(Points);
+    m_vArray.resize(pixelWidth * pixelHeight);
+    m_vArray.create(); 
 }
 
 void ComplexPlane::draw(RenderTarget& target, RenderStates states) const{
@@ -19,18 +19,18 @@ void ComplexPlane::draw(RenderTarget& target, RenderStates states) const{
 //update render
 
 void ComplexPlane::zoomIn(){
-    m_zoomCount++;
-    float x_size = BASE_WIDTH * pow(BASE_ZOOM, m_zoomCount);
-    float y_size = BASE_HEIGHT * m_aspectRatio * pow(BASE_ZOOM, m_zoomCount);
+    m_ZoomCount++;
+    float x_size = BASE_WIDTH * pow(BASE_ZOOM, m_ZoomCount);
+    float y_size = BASE_HEIGHT * m_aspectRatio * pow(BASE_ZOOM, m_ZoomCount);
     m_plane_size = {x_size, y_size};
-    m_State = State::CALCULATING;
+    m_state = State::CALCULATING;
 }
 
 void ComplexPlane::zoomOut(){
-  m_zoomCount--;
-  float x_size = BASE_WIDTH * pow(BASE_ZOOM, m_zoomCount);
-  float y_size = BASE_HEIGHT * m_aspectRatio * pow(BASE_ZOOM, m_zoomCount);
+  m_ZoomCount--;
+  float x_size = BASE_WIDTH * pow(BASE_ZOOM, m_ZoomCount);
+  float y_size = BASE_HEIGHT * m_aspectRatio * pow(BASE_ZOOM, m_ZoomCount);
   m_plane_size = {x_size, y_size};
-  m_State = State::CALCULATING;
+  m_state = State::CALCULATING;
 }
 
