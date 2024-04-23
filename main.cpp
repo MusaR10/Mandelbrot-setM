2262418530761ed7da59ab39cd4affb4d3bc4fd5
Karnpreet2 <166642153+Karnpreet2@users.noreply.github.com>
2024-04-22 20:26:01 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include "ComplexPlane.h"
 #include <iostream>
+#include <SFML/Graphics.hpp>
 
 using namespace sf;
 using namespace std;
@@ -14,8 +15,8 @@ int main() {
   ComplexPlane complexPlane(pixelWidth, pixelHeight);
 
   Font font;
-
-  if (!font.loadFromFile("KOMIKAP.TTF")) {
+  font.loadFromFile("KOMIKAP.ttf")
+  if (!font.loadFromFile("KOMIKAP.ttf")) {
     cerr << "Something wrong with font" << endl;
     return 1;
   }
@@ -27,14 +28,17 @@ int main() {
   text.setFillColor(Color::White);
   text.setStyle(Text::Bold);
 
-  window.draw(text);
-  while (window.isOpen()) {
-    
-    while (window.pollEvent(event)) {
+  while (window.isOpen()) 
+  {
+    Event event;
+    while (window.pollEvent(event)) 
+    {
       if (event.type == sf::Event::Closed) {window.close();}
 
-      if (event.type == sf::Event::MouseButtonPressed) {
-        if (event.mouseButton.button == sf::Mouse::Left) {
+      if (event.type == sf::Event::MouseButtonPressed) 
+      {
+        if (event.mouseButton.button == sf::Mouse::Left) 
+        {
           cout << "Left click = Zooming in." << endl;
           cout << "Mouse x: " << event.mouseButton.x << endl;
           cout << "Mouse y: " << event.mouseButton.y << endl;
@@ -44,32 +48,35 @@ int main() {
           Vector2i(event.mouseButton.x, event.mouseButton.y));
         }
        
-      else if (event.mouseButton.button == sf::Mouse::Right) {
+        else if (event.mouseButton.button == sf::Mouse::Right) 
+        {
         cout << "the right button was pressed" << endl;
         cout << "mouse x: " << event.mouseButton.x << endl;
         cout << "mouse y: " << event.mouseButton.y << endl;
 
         complexPlane.zoomOut();
         complexPlane.setCenter(
-            Vector2i(event.mouseButton.x, event.mouseButton.y));
+        Vector2i(event.mouseButton.x, event.mouseButton.y));
+        }
       }
-
-      if (event.type == sf::Event::MouseMoved) {
-        complexPlane.setMouseLocation(
-            Vector2i(event.mouseMove.x, event.mouseMove.y));
+      else if (event.type == sf::Event::MouseMoved) 
+      {
+        complexPlane.setMouseLocation(Vector2i(event.mouseMove.x, event.mouseMove.y));
       }
+        
     }
 
-    if (Keyboard::isKeyPressed(Keyboard::Escape)) {
+    if (Keyboard::isKeyPressed(Keyboard::Escape)) 
+    {
       window.close();
     }
 
     complexPlane.updateRender();
     complexPlane.loadText(text);
+      
     window.clear();
     window.draw(complexPlane);
     window.draw(text);
     window.display();
   }
-    return 0;
-  }
+}
