ce11dde49b3260eda31186bd76fdec3b8185ddd4
Karnpreet2 <166642153+Karnpreet2@users.noreply.github.com>
2024-04-25 00:47:22 -0700

    Update ComplexPlane.cpp

--- ComplexPlane.cpp
+++ ComplexPlane.cpp
@@ -3,22 +3,34 @@
 #include <cmath>
 
 ComplexPlane::ComplexPlane(int pixelWidth, int pixelHeight){
-    m_pixel_size = {pixelWidth, pixelHeight};
-    m_aspectRatio = (float)pixelHeight / (float)pixelWidth;
-    m_plane_center = {0, 0};
-    m_plane_size = {BASE_WIDTH, BASE_HEIGHT * m_aspectRatio};
-    m_ZoomCount = 0;
-    m_state = State::CALCULATING;
-    m_vArray = VertexArray(Points, pixelWidth*pixelHeight);
-    cout<<m_plane_size.x << " " << m_plane_size.y << endl;
+	m_pixel_size = {pixelWidth, pixelHeight};
+	m_aspectRatio = (float)pixelHeight / (float)pixelWidth;
+	m_plane_center = {0, 0};
+	m_plane_size = {BASE_WIDTH, BASE_HEIGHT * m_aspectRatio};
+	m_ZoomCount = 0;
+	m_state = State::CALCULATING
+	m_vArray = setPrimitiveType(Points);
+	m_vArray.resize(pixelWidth*pixelHeight);
 }
 
 void ComplexPlane::draw(RenderTarget& target, RenderStates states) const{
     target.draw(m_vArray);
 }
 
-//update render
-
+/*update render
+void ComplexPlane::updateRender(){
+if m_state == State::CALCULATING{
+	for int i/i<y
+ 		for int j/ j<x
+   		m_vArray[...]
+     		Vector2f position = mapPTC(j,i);
+       		int iteration = countiterations(position);
+	 	iterate to r,g,b
+   		add color to vArray
+        state::Displaying
+}
+}
+*/
 void ComplexPlane::zoomIn(){
     m_ZoomCount++;
     float x_size = BASE_WIDTH * (pow(BASE_ZOOM, m_ZoomCount));
